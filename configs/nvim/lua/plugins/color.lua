78645b200ffc502240ac601e125e5fdba41ed85b
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2026-06-25 06:23:16 +0000

    using catppuccin colors in nvim

--- configs/nvim/lua/plugins/color.lua
+++ configs/nvim/lua/plugins/color.lua
@@ -1,8 +1,18 @@
+-- return {
+-- 	"rose-pine/neovim",
+-- 	name = "rose-pine",
+-- 	config = function()
+--     require("rose-pine").setup()
+-- 		vim.cmd("colorscheme rose-pine")
+-- 	end
+-- }
+
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-    require("rose-pine").setup()
-		vim.cmd("colorscheme rose-pine")
-	end
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup()
+		vim.cmd("colorscheme catppuccin-nvim")
+  end
 }
