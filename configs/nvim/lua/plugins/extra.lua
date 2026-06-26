b0336477b755065e1b4c36738414fa9f33cf38b7
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2026-06-26 07:33:20 +0000

    done with nvim plugin

--- configs/nvim/lua/plugins/extra.lua
+++ configs/nvim/lua/plugins/extra.lua
@@ -20,46 +20,4 @@ return {
       require("nvim-highlight-colors").setup()
     end,
   },
-
-  -- mini-pick
-  -- {
-  --   'nvim-mini/mini.pick',
-  --   version = '*',
-  --   config = function()
-  --     local pick = require("mini.pick")
-  --     pick.setup({
-  --       mappings = {
-  --         move_down = '<C-j>',
-  --         move_up   = '<C-k>',
-  --         preview   = '<C-p>',
-  --       },
-  --       -- Window related options
-  --       window = {
-  --         config = nil,
-  --
-  --         prompt_prefix = '# ',
-  --       },
-  --     })
-  --
-  --     vim.keymap.set("n", "<leader><leader>", function()
-  --       pick.builtin.files()
-  --     end, { desc = "Find Files" })
-  --
-  --     vim.keymap.set("n", "<leader>fg", function()
-  --       pick.builtin.grep_live()
-  --     end, { desc = "Live Grep" })
-  --
-  --     vim.keymap.set("n", "<leader>fb", function()
-  --       pick.builtin.buffers()
-  --     end, { desc = "Buffers" })
-  --
-  --     vim.keymap.set("n", "<leader>fh", function()
-  --       pick.builtin.help()
-  --     end, { desc = "Help Tags" })
-  --
-  --     vim.keymap.set("n", "<leader>fr", function()
-  --       pick.builtin.resume()
-  --     end, { desc = "Resume Picker" })
-  --   end
-  -- },
 }
--- configs/nvim/lua/plugins/finder.lua
+++ configs/nvim/lua/plugins/finder.lua
@@ -58,3 +58,58 @@ return {
     fzf.register_ui_select()
   end,
 }
+
+-- return
+-- {
+--   'nvim-telescope/telescope.nvim',
+--   version = '*',
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     "nvim-telescope/telescope-ui-select.nvim",
+--   },
+--   config = function()
+--     local actions = require("telescope.actions")
+--     local builtin = require("telescope.builtin")
+--
+--     require("telescope").setup({
+--       defaults = {
+--         dynamic_preview_title = true,
+--         layout_strategy = "horizontal",
+--         borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+--         mappings = {
+--           i = {
+--             ["<C-j>"] = actions.move_selection_next,
+--             ["<C-k>"] = actions.move_selection_previous,
+--           },
+--           n = {
+--             ["<C-j>"] = actions.move_selection_next,
+--             ["<C-k>"] = actions.move_selection_previous,
+--           },
+--         },
+--       },
+--       extensions = {
+--         ["ui-select"] = {
+--           require("telescope.themes").get_dropdown({}),
+--         },
+--         fzf = {
+--           fuzzy = true,
+--           override_generic_sorter = true,
+--           override_file_sorter = true,
+--           case_mode = "smart_case",
+--         },
+--       },
+--
+--     })
+--
+--     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+--     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+--     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+--     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+--     vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, { desc = 'Telescope find old files ' })
+--     vim.keymap.set("n", "<leader>fv", builtin.current_buffer_fuzzy_find)
+--     vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = 'Multi-selections of the previous picker' })
+--
+--     require("telescope").load_extension("ui-select")
+--   end,
+-- }
+--
