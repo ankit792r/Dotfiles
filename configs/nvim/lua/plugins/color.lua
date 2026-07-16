aae305cde82ed98098b4c7a61bffdfe6aa44c760
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2026-07-16 06:28:20 +0000

    going to rose pine

--- configs/nvim/lua/plugins/color.lua
+++ configs/nvim/lua/plugins/color.lua
@@ -1,39 +1,44 @@
--- return {
--- 	"rose-pine/neovim",
--- 	name = "rose-pine",
--- 	config = function()
---     require("rose-pine").setup()
--- 		vim.cmd("colorscheme rose-pine")
--- 	end
--- }
-
 return {
-	-- {
-	-- 	"catppuccin/nvim",
-	-- 	name = "catppuccin",
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		require("catppuccin").setup({
-	-- 			transparent_background = true,
-	-- 			float = {
-	-- 				transparent = true, -- enable transparent floating windows
-	-- 				solid = false, -- use solid styling for floating windows, see |winborder|
-	-- 			},
-	-- 		})
-	-- 		vim.cmd("colorscheme catppuccin-nvim")
-	-- 	end,
-	-- },
-	{
-		"bluz71/vim-moonfly-colors",
-		name = "moonfly",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.g.moonflyItalics = true
-			vim.o.pumborder = "single"
-			vim.g.moonflyTransparent = true
-			vim.g.moonflyNormalFloat = true
-			vim.cmd("colorscheme moonfly")
-		end,
-	},
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		require("rose-pine").setup({
+			styles = {
+				transparency = true,
+			},
+		})
+		vim.o.pumborder = "single"
+		vim.cmd("colorscheme rose-pine")
+	end,
 }
+
+-- return {
+-- {
+-- 	"catppuccin/nvim",
+-- 	name = "catppuccin",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("catppuccin").setup({
+-- 			transparent_background = true,
+-- 			float = {
+-- 				transparent = true, -- enable transparent floating windows
+-- 				solid = false, -- use solid styling for floating windows, see |winborder|
+-- 			},
+-- 		})
+-- 		vim.cmd("colorscheme catppuccin-nvim")
+-- 	end,
+-- },
+-- {
+-- 	"bluz71/vim-moonfly-colors",
+-- 	name = "moonfly",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		vim.g.moonflyItalics = true
+-- 		vim.o.pumborder = "single"
+-- 		vim.g.moonflyTransparent = true
+-- 		vim.g.moonflyNormalFloat = true
+-- 		vim.cmd("colorscheme moonfly")
+-- 	end,
+-- },
+-- }
