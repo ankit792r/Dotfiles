a8129f7ce72dd6d054093f6cc3e084d5bf5aa71f
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2026-07-26 20:26:47 +0530

    updated nvim theme

--- configs/nvim/lua/plugins/themes.lua
+++ configs/nvim/lua/plugins/themes.lua
@@ -1,31 +1,40 @@
 return {
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			require("rose-pine").setup({
-				styles = {
-					transparency = true,
-				},
-			})
-			vim.o.pumborder = "single"
-			-- vim.cmd("colorscheme rose-pine")
+  -- {
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
+  --   config = function()
+  --     require("rose-pine").setup({
+  --       styles = {
+  --         transparency = true,
+  --       },
+  --     })
+  --     vim.o.pumborder = "single"
+  --     -- vim.cmd("colorscheme rose-pine")
+  --
+  --     -- vim.cmd("colorscheme rose-pine-main")
+  --   end,
+  -- },
 
-			vim.cmd("colorscheme rose-pine-main")
-		end,
-	},
-
-	{
-		"bluz71/vim-moonfly-colors",
-		name = "moonfly",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			-- vim.g.moonflyItalics = true
-			-- vim.o.pumborder = "single"
-			-- vim.g.moonflyTransparent = true
-			-- vim.g.moonflyNormalFloat = true
-			-- vim.cmd("colorscheme moonfly")
-		end,
-	},
+  {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.moonflyItalics = true
+      vim.o.pumborder = "single"
+      vim.g.moonflyTransparent = true
+      vim.g.moonflyNormalFloat = true
+      vim.cmd("colorscheme moonfly")
+    end,
+  },
+  -- {
+  --   'vague-theme/vague.nvim',
+  --   config = function()
+  --     require("vague").setup({
+  --       transparent = true
+  --     })
+  --     -- vim.cmd.colorscheme('vague')
+  --   end,
+  -- }
 }
