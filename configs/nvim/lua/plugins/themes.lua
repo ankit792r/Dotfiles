f11615f420ddb3a543ee664af276f7796b2ef02f
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2026-08-20 12:30:35 +0000

    added vague theme

--- configs/nvim/lua/plugins/themes.lua
+++ configs/nvim/lua/plugins/themes.lua
@@ -10,7 +10,19 @@ return {
       vim.g.moonflyTransparent = true
       vim.g.moonflyNormalFloat = true
       vim.g.moonflyWinSeparator = 2
-      vim.cmd("colorscheme moonfly")
+      -- vim.cmd("colorscheme moonfly")
     end,
+  },
+  {
+    "vague-theme/vague.nvim",
+    config = function()
+      require('vague').setup({
+        transparent = true,
+        bold = true,
+        italic = true,
+      })
+
+      vim.cmd("colorscheme vague")
+    end
   },
 }
