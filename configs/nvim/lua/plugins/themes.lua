caf7f67b271b30c4b90828046f6a9c7d5694b6b2
Ankit Prajapati <77499177+ankit792r@users.noreply.github.com>
2026-09-02 05:30:14 +0000

    added vague theme in nvim

--- configs/nvim/lua/plugins/themes.lua
+++ configs/nvim/lua/plugins/themes.lua
@@ -1,18 +1,18 @@
 return {
-  {
-    "bluz71/vim-moonfly-colors",
-    name = "moonfly",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.moonflyItalics = true
-      vim.o.pumborder = "single"
-      vim.g.moonflyTransparent = true
-      vim.g.moonflyNormalFloat = true
-      vim.g.moonflyWinSeparator = 2
-      -- vim.cmd("colorscheme moonfly")
-    end,
-  },
+  -- {
+  --   "bluz71/vim-moonfly-colors",
+  --   name = "moonfly",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.g.moonflyItalics = true
+  --     vim.o.pumborder = "single"
+  --     vim.g.moonflyTransparent = true
+  --     vim.g.moonflyNormalFloat = true
+  --     vim.g.moonflyWinSeparator = 2
+  --     -- vim.cmd("colorscheme moonfly")
+  --   end,
+  -- },
   {
     "vague-theme/vague.nvim",
     config = function()
@@ -22,33 +22,33 @@ return {
         italic = true,
       })
 
-      -- vim.cmd("colorscheme vague")
+      vim.cmd("colorscheme vague")
     end
   },
 
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-      require("rose-pine").setup({
-        variant = "main",
-        dark_variant = "main",
-        dim_inactive_windows = false,
-        extend_background_behind_borders = true,
-
-        enable = {
-          terminal = true,
-          legacy_highlights = true,
-          migrations = true,
-        },
-
-        styles = {
-          bold = true,
-          italic = true,
-          transparency = true,
-        },
-      })
-      vim.cmd("colorscheme rose-pine")
-    end
-  }
+  -- {
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
+  --   config = function()
+  --     require("rose-pine").setup({
+  --       variant = "main",
+  --       dark_variant = "main",
+  --       dim_inactive_windows = false,
+  --       extend_background_behind_borders = true,
+  --
+  --       enable = {
+  --         terminal = true,
+  --         legacy_highlights = true,
+  --         migrations = true,
+  --       },
+  --
+  --       styles = {
+  --         bold = true,
+  --         italic = true,
+  --         transparency = true,
+  --       },
+  --     })
+  --     vim.cmd("colorscheme rose-pine")
+  --   end
+  -- },
 }
